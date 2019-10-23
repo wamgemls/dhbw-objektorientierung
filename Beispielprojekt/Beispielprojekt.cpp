b38d166520ecfc12368b2e1819f06aba2ffb742c
Max Leon Sperling <wamgemls@outlook.com>
2019-10-23 17:10:27 +0200

    meister

--- Beispielprojekt/Beispielprojekt.cpp
+++ Beispielprojekt/Beispielprojekt.cpp
@@ -19,7 +19,7 @@ public:
 	GameWindow()
 		: Window(800, 600)
 	{
-		set_caption("Gosu Tutorial Game mit test");
+		set_caption("Gosu Tutorial Game mit test56");
 	}
 
 	// wird bis zu 60x pro Sekunde aufgerufen.
