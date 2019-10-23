049d261872d1b9c0eaf76067768e20ebc04f266e
kocot.philip <kocot.philip@348A11>
2019-10-23 17:12:38 +0200

    spast

--- Beispielprojekt/Beispielprojekt.cpp
+++ Beispielprojekt/Beispielprojekt.cpp
@@ -19,7 +19,7 @@ public:
 	GameWindow()
 		: Window(800, 600)
 	{
-		set_caption("Gosu Tutorial Game mit test56");
+		set_caption("Gosu Tutorial Game mit AAKJHVKLnkljhtest56");
 	}
 
 	// wird bis zu 60x pro Sekunde aufgerufen.
