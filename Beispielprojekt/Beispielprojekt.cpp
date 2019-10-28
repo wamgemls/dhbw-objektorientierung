c8718e7735967f1e79ec191d1aae692c4b6fe49e
Max Leon Sperling <wamgemls@outlook.com>
2019-10-28 14:07:48 +0100

    yolo

--- Beispielprojekt/Beispielprojekt.cpp
+++ Beispielprojekt/Beispielprojekt.cpp
@@ -19,13 +19,16 @@ typedef std::vector<Gosu::Image> Animation; // Animation Def.
 class Player
 {
 	Gosu::Image bild;
-	double pos_x, pos_y, vel_x, vel_y, angle;
+	
 
 public:
+
+	double pos_x, pos_y, vel_x, vel_y, angle,vfaktortest;
+
 	Player()
 		: bild("car.png")
 	{
-		pos_x = pos_y = vel_x = vel_y = angle = 0;
+		pos_x = pos_y = vel_x = vel_y = angle = vfaktortest = 0;
 	}
 
 	void warp(double x, double y) // Teleport an Postition x y
@@ -36,24 +39,66 @@ public:
 
 	void turn_left()
 	{
-		angle -= 1.5;
+		if (vfaktortest != 0) {
+
+			angle -= 1.5;
+
+		}
+			
 	}
 
 	void turn_right()
 	{
-		angle += 1.5;
+		if (vfaktortest != 0) {
+
+			angle += 1.5;
+
+		}
+	}
+
+	void accelerate() {
+		
+		vfaktortest = vfaktortest + 0.2;
+
+		if (vfaktortest >= 8)
+		{
+			vfaktortest = 8;
+		}
+
+	}
+
+	void reverse() {
+
+		vfaktortest = vfaktortest - 0.4;
+
+		if (vfaktortest <= -2)
+		{
+			vfaktortest = -2;
+		}
+
+	}
+
+	void deceleration() {
+
+		vfaktortest = vfaktortest * 0.97;
+
+		if (abs(vfaktortest) < 0.3)
+		{
+			vfaktortest = 0;
+		}
+
 	}
 
 	double offsetx()
 	{
-		return Gosu::offset_x(angle - 90, vfaktor); // Geschwindigkeit X_Richtung
+		return Gosu::offset_x(angle - 90, vfaktortest); // Geschwindigkeit X_Richtung
 
 	}
 
 	double offsety()
 	{
 
-		return Gosu::offset_y(angle - 90, vfaktor); // Geschwindigkeit Y_Richtung
+		return Gosu::offset_y(angle - 90, vfaktortest); // Geschwindigkeit Y_Richtung
 	}
 
 
@@ -66,13 +111,43 @@ public:
 
 	void draw() const
 	{
-		bild.draw_rot(pos_x, pos_y, 0.5, angle, 0.7, 0.5); // PNG-Center
+		bild.draw_rot(pos_x, pos_y, 0.5, angle, 0.5, 0.5); // PNG-Center
 	}
+
+	
+	void kollision() {
+
+		vfaktortest = 0;
+
+	}
+
+
 };
+
+class hitbox : public Player {
+
+public:
+
+
+
+
+
+};
+
+class projektil {
+
+
+
+
+};
+
+
 
 class GameWindow : public Gosu::Window
 {
-	Player p1;
+	int kolrad = 80; //Kollisionsradius
+	
+	Player p1, p2;
 
 public:
 	Gosu::Image bild;
@@ -81,56 +156,91 @@ public:
 	{
 		set_caption("Need for Gosu");
 		p1.warp(400, 300);
+		p2.warp(600, 700);
 	}
 
 	void update() override
 	{
-		if (((Gosu::Input::down(Gosu::KB_LEFT)) || (Gosu::Input::down(Gosu::GP_LEFT))) && vfaktor != 0) // Links (Pfeiltase) (Steuerkreuz oder Stick)
+		if (((Gosu::Input::down(Gosu::KB_LEFT)) || (Gosu::Input::down(Gosu::GP_LEFT)))) // Links (Pfeiltase) (Steuerkreuz oder Stick)
 		{
 			p1.turn_left();
 		}
 		
-		if (((Gosu::Input::down(Gosu::KB_RIGHT)) || (Gosu::Input::down(Gosu::GP_RIGHT))) && vfaktor != 0) // Rechts (Pfeiltase) (Steuerkreuz oder Stick)
+		if (((Gosu::Input::down(Gosu::KB_RIGHT)) || (Gosu::Input::down(Gosu::GP_RIGHT)))) // Rechts (Pfeiltase) (Steuerkreuz oder Stick)
 		{
 			p1.turn_right();
 		}
 
 		if ((Gosu::Input::down(Gosu::KB_UP)) || (Gosu::Input::down(Gosu::GP_BUTTON_0))) // Vorwärts (Pfeiltase) (A/X)
 		{ 
-			vfaktor = vfaktor + 0.2;
+			p1.accelerate();
+		}
 
-			if (vfaktor >= 8)
-			{
-				vfaktor = 8;
-			}
+		if (Gosu::distance(p1.pos_x, p1.pos_y, p2.pos_x, p2.pos_y) < kolrad) {
+
+			p1.kollision();
+
 		}
 
 		if ((Gosu::Input::down(Gosu::KB_DOWN)) || (Gosu::Input::down(Gosu::GP_BUTTON_1))) // Rückwärts (Pfeiltase) (B/O)
 		{ 
-			vfaktor = vfaktor - 0.4;
-
-			if (vfaktor <= -2)
-			{
-				vfaktor = -2;
-			}
+			p1.reverse();
 		}
 
 		if (!Gosu::Input::down(Gosu::KB_UP) && !Gosu::Input::down(Gosu::KB_DOWN)) // Entschleunigung
 		{
-			vfaktor = vfaktor * 0.97;
-
-			if (abs(vfaktor) < 0.3)
-			{
-				vfaktor = 0;
-			}
+			p1.deceleration();
 		}
 
 		p1.move();
+
+		if (((Gosu::Input::down(Gosu::KB_A)) || (Gosu::Input::down(Gosu::GP_LEFT)))) // Links (Pfeiltase) (Steuerkreuz oder Stick)
+		{
+			p2.turn_left();
+		}
+
+		if (((Gosu::Input::down(Gosu::KB_D)) || (Gosu::Input::down(Gosu::GP_RIGHT)))) // Rechts (Pfeiltase) (Steuerkreuz oder Stick)
+		{
+			p2.turn_right();
+		}
+
+		if ((Gosu::Input::down(Gosu::KB_W)) || (Gosu::Input::down(Gosu::GP_BUTTON_0))) // Vorwärts (Pfeiltase) (A/X)
+		{
+			p2.accelerate();
+		}
+
+		if (Gosu::distance(p1.pos_x, p1.pos_y, p2.pos_x, p2.pos_y) < kolrad) {
+
+			p2.kollision();
+			
+		}
+
+		if ((Gosu::Input::down(Gosu::KB_S)) || (Gosu::Input::down(Gosu::GP_BUTTON_1))) // Rückwärts (Pfeiltase) (B/O)
+		{
+			p2.reverse();
+		}
+
+		if (!Gosu::Input::down(Gosu::KB_W) && !Gosu::Input::down(Gosu::KB_S)) // Entschleunigung
+		{
+			p2.deceleration();
+		}
+
+		p2.move();
+
+
+		std::cout << p1.pos_x << std::endl;
+
+		
+
 	}
+
+
+
 
 	void draw() override
 	{
 		p1.draw(); // Car
+		p2.draw();
 		bild.draw(0,0,0.0,1,1); // Racetrack
 	}
 };
