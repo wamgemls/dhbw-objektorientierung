80c4501991bdd838c9ecd74ef622a4a6419ec69f
Max Leon Sperling <wamgemls@outlook.com>
2019-10-31 18:18:54 +0100

    nixs

--- Beispielprojekt/Beispielprojekt.cpp
+++ Beispielprojekt/Beispielprojekt.cpp
@@ -358,6 +358,25 @@ public:
 		
 		}
 
+		if (Gosu::distance(p1.x(), p1.y(), p2.x(), p2.y()) < kolrad) {
+
+			p2.collision();
+
+			if (p2.firstcollision() == true) {
+
+				
+
+				p2.firstcollisionOFF();
+
+			}
+		}
+
+		if (Gosu::distance(p1.x(), p1.y(), p2.x(), p2.y()) > 60) {
+
+			p2.firstcollisionON();
+
+		}
+
 
 
 
